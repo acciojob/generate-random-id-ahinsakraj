e4dc30d52ec406d597e1b4da1a86c7adf7a1b307
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 16:51:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	let ans='';
+	for(let i=0;i<l;i++){
+		if(i%2===0)ans+='1';
+		else ans+='a';
+	}
+	return ans;
 }
 
 // Do not change the code below.
